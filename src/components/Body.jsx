1e13c1fbb255b066ba8c4ab2b3339efc12e0131d
gela pipia <gelapipia10@gmail.com>
2023-08-23 20:31:08 +0400

    added more functionality: todo item deletion and item marked as done, also added little styles

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,26 +13,67 @@ export default function Body() {
     }
 
     setItems((odlItems) => {
-      return [...odlItems, { text: newItem, id: uuid() }];
+      return [...odlItems, { text: newItem, id: uuid(), isDone: false }];
     });
 
     setNewItem("");
   };
 
+  //helper function for adding style like this toDo is done
+  const markAsDone = (id) => {
+    setItems((oldItems) => {
+      const markedItems = oldItems.map((item) => {
+        if (item.id == id) {
+          return { ...item, isDone: !item.isDone };
+        }
+        return item;
+      });
+      return markedItems;
+    });
+  };
+
+  // function for deleting items
+  const deleteItem = (id) => {
+    setItems((oldItems) => {
+      const filteredItems = oldItems.filter((item) => {
+        if (item.id === id) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      return filteredItems;
+    });
+  };
+
   return (
     <div className="Body">
-      <input
-        type="text"
-        className="todo-input"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-      />
-      <button className="add-btn" onClick={() => addItem()}>
-        ADD
-      </button>
+      <div className="input-container">
+        <input
+          type="text"
+          className="todo-input"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button className="add-btn" onClick={() => addItem()}>
+          ADD
+        </button>
+      </div>
       <ul>
         {items.map((i) => {
-          return <li key={i.id}>{i.text}</li>;
+          return (
+            <div key={i.id} className="todo-item">
+              <li
+                style={{
+                  textDecoration: i.isDone && "line-through",
+                  fontStyle: i.isDone && "italic",
+                }}
+                onClick={() => markAsDone(i.id)}>
+                {i.text}
+              </li>
+              <button onClick={() => deleteItem(i.id)}>❌</button>
+            </div>
+          );
         })}
       </ul>
     </div>
